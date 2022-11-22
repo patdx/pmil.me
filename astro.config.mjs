e51c5d08d872f541ca9d3f0f3ae6242e359bb847
Patrick Miller <pamiller.pdx@gmail.com>
2022-11-22 12:47:06 +0900

    Remove trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,28 @@
-import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import image from "@astrojs/image";
+import image from '@astrojs/image';
 
 // https://astro.build/config
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: 'https://pmil.me/',
+  build: {
+    format: 'file',
+  },
   integrations: [
     mdx(),
     sitemap(),
     tailwind(),
     image({
-      serviceEntryPoint: "@astrojs/image/sharp",
+      serviceEntryPoint: '@astrojs/image/sharp',
     }),
     react(),
   ],
